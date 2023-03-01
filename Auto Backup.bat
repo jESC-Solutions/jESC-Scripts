09b1f9751b319f5e598b61f66d131866cd2c56d9
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-02-28 18:10:43 -0800

    Create Auto Backup.bat

---
+++ Auto Backup.bat
@@ -0,0 +1,3 @@
+robocopy "C:\My File Backup" "\\10.0.0.14\Back up\AOMEI Backup Files"
+robocopy "C:\Users\johnW10\Documents" "\\10.0.0.14\Back up\Documents" /XA:SH
+robocopy "C:\System Backup(1)" "\\10.0.0.14\Back up\AOMEI Backup Files"
