cd4f4a32da2b6604fcb896e02cc97f184c52a4d2
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-02-28 16:24:42 -0800

    Delete Auto Backup.bat

--- Auto Backup.bat
+++
@@ -1,5 +0,0 @@
-# John's Script for backing up files from Windows to Linux Server
-
-robocopy "C:\My File Backup" "\\10.0.0.14\Back up\AOMEI Backup Files"
-robocopy "C:\Users\johnW10\Documents" "\\10.0.0.14\Back up\Documents" /XA:SH
-robocopy "C:\System Backup (1)" "\\10.0.0.14\Back up\AOMEI Backup Files"
